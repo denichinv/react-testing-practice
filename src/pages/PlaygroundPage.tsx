87898281498b7bdede05a06cd55976874c845249
Vilizar Denichin <denichinv@gmail.com>
2025-12-06 16:18:28 +0000

    test: Complete behavioral tests for ExpandableText component

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eveniet, cum quidem, provident sint sequi repudiandae explicabo illum vel, aliquid possimus dolorem! Dolore perspiciatis harum a. Rerum, possimus culpa non et error magni quos, odit consequatur nostrum amet minus atque esse numquam perferendis ratione eaque suscipit pariatur neque quas illum obcaecati quo quibusdam autem? Optio accusamus vel, illum dolor sunt mollitia perspiciatis officia voluptatem animi tenetur eaque amet voluptas adipisci quibusdam. Iusto fugit reprehenderit laborum dignissimos, ratione optio id porro reiciendis aliquid cum laboriosam eos! Corporis ab facilis vel repudiandae excepturi, expedita aspernatur accusantium libero debitis unde nesciunt nisi quae." />
+  );
 };
 
 export default PlaygroundPage;
---
+++ tests/components/ExpandableText.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import ExpandableText from "../../src/components/ExpandableText";
+import userEvent from "@testing-library/user-event";
+
+describe("ExpandableText", () => {
+  it("should render short text without a button for expanding the text ", () => {
+    render(<ExpandableText text="Lorem ipsum " />);
+    expect(screen.getByRole("article")).toHaveTextContent("Lorem ipsum");
+    expect(screen.getAllByRole("button")).not.toBeInTheDocument();
+  });
+  it("should expand text when Show More button is clicked ", async () => {
+    const text =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel omnis quaerat nihil eaque odit et possimus corrupti, aperiam saepe debitis eveniet sunt natus non asperiores maxime adipisci dicta? Veniam quas distinctio expedita nihil quibusdam voluptatum impedit, neque consequatur quia nisi tenetur necessitatibus cumque fuga dignissimos aliquid repellat laboriosam error! Mollitia assumenda quos aliquid consequuntur molestias, sint placeat ex neque dolorum possimus id nihil atque inventore quibusdam voluptatum reiciendis deleniti, maiores molestiae perferendis? Exercitationem, soluta, reprehenderit aliquid minus incidunt, saepe facere ducimus quaerat est velit blanditiis voluptatum earum temporibus expedita sunt. Nostrum ex deserunt atque assumenda mollitia. Nihil adipisci voluptates quos.";
+    render(<ExpandableText text={text} />);
+    const truncText = text.substring(0, 255) + "...";
+    const button = screen.getByRole("button");
+    const user = userEvent.setup();
+
+    expect(screen.getByText(truncText)).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Show More");
+
+    await user.click(button);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+    expect(button).toHaveTextContent("Show Less");
+  });
+  it("should collapse text when Show Less button is clicked ", async () => {
+    const text =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel omnis quaerat nihil eaque odit et possimus corrupti, aperiam saepe debitis eveniet sunt natus non asperiores maxime adipisci dicta? Veniam quas distinctio expedita nihil quibusdam voluptatum impedit, neque consequatur quia nisi tenetur necessitatibus cumque fuga dignissimos aliquid repellat laboriosam error! Mollitia assumenda quos aliquid consequuntur molestias, sint placeat ex neque dolorum possimus id nihil atque inventore quibusdam voluptatum reiciendis deleniti, maiores molestiae perferendis? Exercitationem, soluta, reprehenderit aliquid minus incidunt, saepe facere ducimus quaerat est velit blanditiis voluptatum earum temporibus expedita sunt. Nostrum ex deserunt atque assumenda mollitia. Nihil adipisci voluptates quos.";
+    render(<ExpandableText text={text} />);
+    const truncText = text.substring(0, 255) + "...";
+    const button = screen.getByRole("button");
+    const user = userEvent.setup();
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Show Less");
+
+    await user.click(button);
+
+    expect(button).toHaveTextContent("Show More");
+    expect(screen.getByText(truncText)).toBeInTheDocument();
+  });
+});
